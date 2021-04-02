2815dc6401f517dac589ea18cf4e381fcf413a72
Ariel Gavrielov <arielariel1010@gmail.com>
2021-04-02 14:46:14 +0300

    Update CCVibrate.m

--- CCVibrate.m
+++ CCVibrate.m
@@ -16,29 +16,29 @@
 
 - (BOOL)isSelected
 {
-  CFPropertyListRef CFSilent = CFPreferencesCopyAppValue(CFSTR("silent-vibrate"), CFSTR("com.apple.springboard"));
-  CFPropertyListRef CFRing = CFPreferencesCopyAppValue(CFSTR("ring-vibrate"), CFSTR("com.apple.springboard"));
-  _selected = CFBooleanGetValue((CFBooleanRef)CFSilent) && CFBooleanGetValue((CFBooleanRef)CFRing);
+	CFPropertyListRef CFSilent = CFPreferencesCopyAppValue(CFSTR("silent-vibrate"), CFSTR("com.apple.springboard"));
+  	CFPropertyListRef CFRing = CFPreferencesCopyAppValue(CFSTR("ring-vibrate"), CFSTR("com.apple.springboard"));
+  	_selected = CFBooleanGetValue((CFBooleanRef)CFSilent) && CFBooleanGetValue((CFBooleanRef)CFRing);
 
-  return _selected;
+  	return _selected;
 }
 
 - (void)setSelected:(BOOL)selected
 {
 	_selected = selected;
-  [super refreshState];
-  
-  NSString *sbPath = @"/var/mobile/Library/Preferences/com.apple.springboard.plist";
-  NSMutableDictionary *prefsDict = [[NSMutableDictionary alloc] initWithContentsOfFile:sbPath];
+	[super refreshState];
+	
+	NSString *sbPath = @"/var/mobile/Library/Preferences/com.apple.springboard.plist";
+  	NSMutableDictionary *prefsDict = [[NSMutableDictionary alloc] initWithContentsOfFile:sbPath];
 
-  [prefsDict setObject:[NSNumber numberWithBool:selected] forKey:@"silent-vibrate"];
+  	[prefsDict setObject:[NSNumber numberWithBool:selected] forKey:@"silent-vibrate"];
 	[prefsDict setObject:[NSNumber numberWithBool:selected] forKey:@"ring-vibrate"];
 	[prefsDict writeToFile:sbPath atomically:YES];
 
-  CFPreferencesSetAppValue(CFSTR("silent-vibrate"), selected ? kCFBooleanTrue : kCFBooleanFalse, CFSTR("com.apple.springboard"));
+  	CFPreferencesSetAppValue(CFSTR("silent-vibrate"), selected ? kCFBooleanTrue : kCFBooleanFalse, CFSTR("com.apple.springboard"));
 	CFPreferencesSetAppValue(CFSTR("ring-vibrate"), selected ? kCFBooleanTrue : kCFBooleanFalse, CFSTR("com.apple.springboard"));
 
-  CFPreferencesAppSynchronize(CFSTR("com.apple.springboard"));
+  	CFPreferencesAppSynchronize(CFSTR("com.apple.springboard"));
 	CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.apple.springboard.silent-vibrate.changed"), NULL, NULL, TRUE);
 	CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.apple.springboard.ring-vibrate.changed"), NULL, NULL, TRUE);
 }
